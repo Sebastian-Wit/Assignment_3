20ac1d38e7054576004e201a6041debce251fe54
S <abc123@gmail.com>
2023-03-20 20:43:44 -0400

    Added comments, cleaned up code

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ function addR() {
     let table = document.getElementById("grid");
     numRows ++;
     let newRow = document.createElement("tr");
-    if(numCols == 0) {
+    if(numCols == 0) { //If table is empty
         numCols ++;
-        let newCell = document.createElement("td");
-        newRow.appendChild(newCell);
+        let newCol = document.createElement("td");
+        newRow.appendChild(newCol); //First row and column must be added together
     }
     else{
         for(let i = 0; i < numCols; i++){
-            let newCell = document.createElement("td");
-            newRow.appendChild(newCell); 
+            let newCol = document.createElement("td");
+            newRow.appendChild(newCol); //Add a new cell to each column (adding a new row)
         }
     }
     table.appendChild(newRow);
@@ -26,18 +26,18 @@ function addR() {
 function addC() {
     let table = document.getElementById("grid");
     numCols ++;
-    let rows = table.getElementsByTagName("tr");
-    if(numRows == 0) {
+    if(numRows == 0) { //If table is empty
         numRows ++;
         let newRow = document.createElement("tr");
-        let newCell = document.createElement("td");
-        newRow.appendChild(newCell);
+        let newCol = document.createElement("td");
+        newRow.appendChild(newCol); //Add first cell
         table.appendChild(newRow);
     }
     else{
         for(let i = 0; i < numRows; i++){
-            let newCell = document.createElement("td");
-            rows[i].appendChild(newCell); 
+            let rows = table.getElementsByTagName("tr");
+            let newCol = document.createElement("td");
+            rows[i].appendChild(newCol); //Add a new cell to each row (adding a column)
         }
     }
 }
@@ -45,11 +45,11 @@ function addC() {
 // Remove a row
 function removeR() {
     let table = document.getElementById("grid");
-    if(numRows > 0){
-        table.deleteRow(numRows - 1);
+    if(numRows > 0){ //If table isn't empty
+        table.deleteRow(numRows - 1); //Delete the last row
         numRows --;
         if(numRows == 0){
-            numCols = 0;
+            numCols = 0; //Update the variables if necessary and the table is empty
         }
     }
 }
@@ -58,9 +58,9 @@ function removeR() {
 function removeC() {
     let table = document.getElementById("grid");
     let rows = table.getElementsByTagName("tr");
-    if(numCols > 0){
+    if(numCols > 0){ //If table isn't empty
         for(let i = 0; i < numRows; i++){
-            rows[i].deleteCell(numCols - 1);
+            rows[i].deleteCell(numCols - 1); //Delete the final column from every row
         }
         numCols --;
         if(numCols == 0){
@@ -83,10 +83,10 @@ function fillU(){
     let table = document.getElementById("grid");
     let rows = table.getElementsByTagName("tr");
     for(let i = 0; i < numRows; i++){
-        for(let j = 0; j < numCols; j++){
-            let cell = rows[i].cells[j];
-            if(!cell.style.backgroundColor || cell.style.backgroundColor == "white"){ //The cells are only set to white after a clear, otherwise they don't have a background color
-                cell.style.backgroundColor = colorSelected;
+        for(let j = 0; j < numCols; j++){ //For every cell
+            let col = rows[i].cells[j];
+            if(!col.style.backgroundColor || col.style.backgroundColor == "white"){ //The cells are only set to white after a clear, otherwise they don't have a background color
+                col.style.backgroundColor = colorSelected;
             }
         }
     }
@@ -98,9 +98,9 @@ function fillAll(){
     let table = document.getElementById("grid");
     let rows = table.getElementsByTagName("tr");
     for(let i = 0; i < numRows; i++){
-        for(let j = 0; j < numCols; j++){
-            let cell = rows[i].cells[j];
-            cell.style.backgroundColor = colorSelected;
+        for(let j = 0; j < numCols; j++){ //For every cell
+            let col = rows[i].cells[j];
+            col.style.backgroundColor = colorSelected; //Update the color to the selected color
         }
     }
 }
@@ -110,9 +110,9 @@ function clearAll(){
     let table = document.getElementById("grid");
     let rows = table.getElementsByTagName("tr");
     for(let i = 0; i < numRows; i++){
-        for(let j = 0; j < numCols; j++){
-            let cell = rows[i].cells[j];
-            cell.style.backgroundColor = "white";
+        for(let j = 0; j < numCols; j++){ //For every cell
+            let col = rows[i].cells[j];
+            col.style.backgroundColor = "white"; //Set the background color to white
         }
     }
 }
